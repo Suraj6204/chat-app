a1a502ed0e799292b127023a330ef20f1fb03a85
Suraj6204 <suraj.2023ug4001@iiitranchi.ac.in>
2026-03-23 12:44:42 +0530

    backend completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,21 +3,24 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import path from "path";
+// import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+// import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
+const app = express();
+
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -26,7 +29,7 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -36,7 +39,7 @@ app.use("/api/auth", authRoutes);
 //   });
 // }
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
