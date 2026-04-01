5454c7c9f14705f59f671ee78ba1f9e249497d71
Suraj6204 <suraj.2023ug4001@iiitranchi.ac.in>
2026-04-01 16:50:31 +0530

    implemented settings ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
 import EmailVerify from './pages/EmailVerify';
+import SettingsPage from './pages/SettingsPage';
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
@@ -38,6 +39,7 @@ const App = () => {
         <Route path="/" element={authUser ? (authUser.isVerified ? <HomePage /> : <Navigate to="/verify-email" />) : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : (authUser.isVerified ? <Navigate to="/" /> : <Navigate to="/verify-email" />)} />
         <Route path="/login" element={!authUser ? <LoginPage /> : (authUser.isVerified ? <Navigate to="/" /> : <Navigate to="/verify-email" />)} />
+        <Route path="/settings" element={<SettingsPage />} />
         <Route path="/verify-email" element={authUser && !authUser.isVerified ? <EmailVerify /> : <Navigate to="/" />} />
       </Routes>
       
